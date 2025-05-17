7645a0667bc544e7a19c48588d3b9cd24fedb0fa
Sergio Fidelis <sergiofidelis31@hotmail.com>
2025-05-16 23:05:40 -0300

    Remove comments

--- main.go
+++ main.go
@@ -9,47 +9,6 @@ import (
 	"github.com/joho/godotenv"
 )
 
-/**
- * POST /shorten
- * {
- *  "url": "https://www.example.com/some/long/url"
- * }
- *
- * {
- *  "id": "1",
- *  "url": "https://www.example.com/some/long/url",
- *  "shortCode": "abc123",
- *  "createdAt": "2021-09-01T12:00:00Z",
- *  "updatedAt": "2021-09-01T12:00:00Z"
- * }
- * status 201, 400
- *
- * GET /shorten/abc123
- * status 200, 404
- *
- * PUT /shorten/abc123
- * {
- * "url": "https://www.example.com/some/updated/url"
- * }
- * status 200,404,400
- *
- * DELETE /shorten/abc123
- * status 204, 404
- *
- * GET /shorten/abc123/stats
- * {
- * "id": "1",
- * "url": "https://www.example.com/some/long/url",
- * "shortCode": "abc123",
- * "createdAt": "2021-09-01T12:00:00Z",
- * "updatedAt": "2021-09-01T12:00:00Z",
- * "accessCount": 10
- * }
- *status: 202, 404
- *
- *
- * status 200, 404
- */
 func main() {
 	loadEnv()
 
